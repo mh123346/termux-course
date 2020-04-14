81e1e699c205fedbff319b40542b94762377de1c
mh123346 <56525549+mh123346@users.noreply.github.com>
2020-04-14 17:29:31 +0200

    Add files via upload

---
+++ Termux_course.py
@@ -0,0 +1,324 @@
+from os import *
+#################
+
+#################
+system ("clear")
+#################
+system ("termux-open https://www.youtube.com/channel/UCr1TEpJXT7F_52aeLttQK_w")
+#################
+#y \033[0;33m
+#r \033[0;31m
+#g \033[0;32m
+#b \033[0;34m
+#################
+print ("\033[0;32m _ ")
+print ("\033[0;32m| |_ ___ _ __ _ __ ___  _   ___  __   \033[0;31m___ ___  _   _ _ __ ___  ___ ")
+print ("\033[0;32m| __/ _ \ '__| '_ ` _ \| | | \ \/ /  \033[0;31m/ __/ _ \| | | | '__/ __|/ _ \ ")
+print ("\033[0;32m| ||  __/ |  | | | | | | |_| |>  <  \033[0;31m| (_| (_) | |_| | |  \__ \  __/ ")
+print ("\033[0;32m \__\___|_|  |_| |_| |_|\__,_/_/\_\  \033[0;31m\___\___/ \__,_|_|  |___/\___| ")
+##################
+print (" ")
+print ("\033[0;31m(1)-\033[0;32mMho")
+system ("sleep 0.3")
+print ("\033[0;31m(2)-\033[0;32mvirtexwhatsapp")
+system ("sleep 0.3")
+print ("\033[0;31m(3)-\033[0;32mscreenshot")
+system ("sleep 0.3")
+print ("\033[0;31m(4)-\033[0;32mwifite.py")
+system ("sleep 0.3")
+print ("\033[0;31m(5)-\033[0;32mhammer")
+system ("sleep 0.3")
+print ("\033[0;31m(6)-\033[0;32mhax4us")
+system ("sleep 0.3")
+print ("\033[0;31m(7)-\033[0;32mweeman")
+system ("sleep 0.3")
+print ("\033[0;31m(8)-\033[0;32mevilimeter")
+system ("sleep 0.3")
+print ("\033[0;31m(9)-\033[0;32mtermux-sudo")
+system ("sleep 0.3")
+print ("\033[0;31m(10)-\033[0;32mLazymux")
+system ("sleep 0.3")
+print ("\033[0;31m(11)-\033[0;32mtermux-ohmyzsh")
+system ("sleep 0.3")
+print ("\033[0;31m(12)-\033[0;32mfb")
+system ("sleep 0.3")
+print ("\033[0;31m(13)-\033[0;32mfacecall")
+system ("sleep 0.3")
+print ("\033[0;31m(14)-\033[0;32mipgeolocation")
+system ("sleep 0.3")
+print ("\033[0;31m(15)-\033[0;32msaycheese")
+system ("sleep 0.3")
+print ("\033[0;31m(16)-\033[0;32mDDos Attack")
+system ("sleep 0.3")
+print ("\033[0;31m(17)-\033[0;32mAmer")
+system ("sleep 0.3")
+print ("\033[0;31m(18)-\033[0;32mTmux-Bunch")
+system ("sleep 0.3")
+print ("\033[0;31m(19)-\033[0;32mnikto")
+system ("sleep 0.3")
+print ("\033[0;31m(20)-\033[0;32mLinux.py")
+system ("sleep 0.3")
+print ("\033[0;31m(21)-\033[0;32mabout us")
+system ("sleep 0.3")
+print ("\033[0;31m(22)-\033[0;32mexit")
+system ("sleep 0.3")
+mh =input ("Choose number:>>>")
+
+
+if mh =="1" :
+    system ("git clone https://github.com/mh123346/Mho.git")
+    system ("apt update & apt upgrade")
+    print("thanks for using our tool")
+    print ("\033[0;32m1-exit")
+    print ("\033[0;32m2-back")
+    ml =input ("choose number>>>")
+    if ml =="1" :
+        exit ()
+    if ml =="2" :
+        system ("python Termux_course.py")
+####################################
+if mh =="2" :
+    system ("git clone https://github.com/MR-404-FOUND/VirtexWhatsapp")
+    system ("apt update & apt upgrade")
+    print("thanks for using our tool")
+    print ("\033[0;32m1-exit")
+    print ("\033[0;32m2-back")
+    ml =input ("choose number>>>")
+    if ml =="1" :
+        exit ()
+    if ml =="2" :
+        system ("python Termux_course.py")
+####################################
+if mh =="3" :
+    system ("git clone https://github.com/Mahmoud514/screenshot")
+    system ("apt update & apt upgrade")
+    print("thanks for using our tool")
+    print ("\033[0;32m1-exit")
+    print ("\033[0;32m2-back")
+    ml =input ("choose number>>>")
+    if ml =="1" :
+        exit ()
+    if ml =="2" :
+        system ("python Termux_course.py")
+####################################
+if mh =="4" :
+    system ("wget https://raw.github.com/derv82/wifite/master/wifite.py")
+    system ("apt update & apt upgrade")
+    print("thanks for using our tool")
+    print ("\033[0;32m1-exit")
+    print ("\033[0;32m2-back")
+    ml =input ("choose number>>>")
+    if ml =="1" :
+        exit ()
+    if ml =="2" :
+        system ("python Termux_course.py")
+####################################
+if mh =="5" :
+    system ("git clone https://github.com/cyweb/hammer.git")
+    system ("apt update & apt upgrade")
+    print("thanks for using our tool")
+    print ("\033[0;32m1-exit")
+    print ("\033[0;32m2-back")
+    ml =input ("choose number>>>")
+    if ml =="1" :
+        exit ()
+    if ml =="2" :
+        system ("python Termux_course.py")
+####################################
+if mh =="6" :
+    system ("git clone https://github.com/Hax4us/java")
+    system ("apt update & apt upgrade")
+    print("thanks for using our tool")
+    print ("\033[0;32m1-exit")
+    print ("\033[0;32m2-back")
+    ml =input ("choose number>>>")
+    if ml =="1" :
+        exit ()
+    if ml =="2" :
+        system ("python Termux_course.py")
+####################################
+if mh =="7" :
+    system ("git clone https://github.com/evait-security/weeman")
+    system ("apt update & apt upgrade")
+    print("thanks for using our tool")
+    print ("\033[0;32m1-exit")
+    print ("\033[0;32m2-back")
+    ml =input ("choose number>>>")
+    if ml =="1" :
+        exit ()
+    if ml =="2" :
+        system ("python Termux_course.py")
+####################################
+if mh =="8" :
+    system ("git clone https://github.com/bitbrute/evillimiter.git")
+    system ("apt update & apt upgrade")
+    print("thanks for using our tool")
+    print ("\033[0;32m1-exit")
+    print ("\033[0;32m2-back")
+    ml =input ("choose number>>>")
+    if ml =="1" :
+        exit ()
+    if ml =="2" :
+        system ("python Termux_course.py")
+####################################
+if mh =="9" :
+    system ("git clone https://github.com/st42/termux-sudo")
+    system ("apt update & apt upgrade")
+    print("thanks for using our tool")
+    print ("\033[0;32m1-exit")
+    print ("\033[0;32m2-back")
+    ml =input ("choose number>>>")
+    if ml =="1" :
+        exit ()
+    if ml =="2" :
+        system ("python Termux_course.py")
+####################################
+if mh =="10" :
+    system ("git clone https://github.com/Gameye98/Lazymux.git")
+    system ("apt update & apt upgrade")
+    print("thanks for using our tool")
+    print ("\033[0;32m1-exit")
+    print ("\033[0;32m2-back")
+    ml =input ("choose number>>>")
+    if ml =="1" :
+        exit ()
+    if ml =="2" :
+        system ("python Termux_course.py")
+####################################
+if mh =="11" :
+    system ("git clone https://github.com/Cabbagec/termux-ohmyzsh")
+    system ("apt update & apt upgrade")
+    print("thanks for using our tool")
+    print ("\033[0;32m1-exit")
+    print ("\033[0;32m2-back")
+    ml =input ("choose number>>>")
+    if ml =="1" :
+        exit ()
+    if ml =="2" :
+        system ("python Termux_course.py")
+####################################
+if mh =="12" :
+    system ("wget clone  http://fasilcm.tk/termux/fb.py")
+    system ("apt update & apt upgrade")
+    print("thanks for using our tool")
+    print ("\033[0;32m1-exit")
+    print ("\033[0;32m2-back")
+    ml =input ("choose number>>>")
+    if ml =="1" :
+        exit ()
+    if ml =="2" :
+        system ("python Termux_course.py")
+####################################
+if mh =="13" :
+    system ("git clone https://github.com/siputra12/fakecall")
+    system ("apt update & apt upgrade")
+    print("thanks for using our tool")
+    print ("\033[0;32m1-exit")
+    print ("\033[0;32m2-back")
+    ml =input ("choose number>>>")
+    if ml =="1" :
+        exit ()
+    if ml =="2" :
+        system ("python Termux_course.py")
+####################################
+if mh =="14" :
+    system ("git clone https://github.com/maldevel/IPGeoLocation")
+    system ("apt update & apt upgrade")
+    print("thanks for using our tool")
+    print ("\033[0;32m1-exit")
+    print ("\033[0;32m2-back")
+    ml =input ("choose number>>>")
+    if ml =="1" :
+        exit ()
+    if ml =="2" :
+        system ("python Termux_course.py")
+####################################
+if mh =="15" :
+    system ("git clone https://github.com/thelinuxchoice/saycheese")
+    system ("apt update & apt upgrade")
+    print("thanks for using our tool")
+    print ("\033[0;32m1-exit")
+    print ("\033[0;32m2-back")
+    ml =input ("choose number>>>")
+    if ml =="1" :
+        exit ()
+    if ml =="2" :
+        system ("python Termux_course.py")
+####################################    	
+if mh =="16" :
+    system ("git clone https://github.com/Stephin-Franklin/DDos-Attack")
+    system ("apt update & apt upgrade")
+    print("thanks for using our tool")
+    print ("\033[0;32m1-exit")
+    print ("\033[0;32m2-back")
+    ml =input ("choose number>>>")
+    if ml =="1" :
+        exit ()
+    if ml =="2" :
+        system ("python Termux_course.py")
+####################################
+if mh =="17" :
+    system ("git clone https://github.com/Amerlaceset/Amer")
+    system ("apt update & apt upgrade")
+    print("thanks for using our tool")
+    print ("\033[0;32m1-exit")
+    print ("\033[0;32m2-back")
+    ml =input ("choose number>>>")
+    if ml =="1" :
+        exit ()
+    if ml =="2" :
+        system ("python Termux_course.py")
+####################################
+if mh =="18" :
+    system ("git clone https://github.com/Hax4us/Tmux-Bunch")
+    system ("apt update & apt upgrade")
+    print("thanks for using our tool")
+    print ("\033[0;32m1-exit")
+    print ("\033[0;32m2-back")
+    ml =input ("choose number>>>")
+    if ml =="1" :
+        exit ()
+    if ml =="2" :
+        system ("python Termux_course.py")
+####################################
+if mh =="19" :
+    system ("git clone https://github.com/sullo/nikto")
+    system ("apt update & apt upgrade")
+    print("thanks for using our tool")
+    print ("\033[0;32m1-exit")
+    print ("\033[0;32m2-back")
+    ml =input ("choose number>>>")
+    if ml =="1" :
+        exit ()
+    if ml =="2" :
+        system ("python Termux_course.py")
+####################################
+if mh =="20" :
+    system ("git clone https://github.com/mh123346/Linux.py.git ")
+    system ("apt update & apt upgrade")
+    print("thanks for using our tool")
+    print ("\033[0;32m1-exit")
+    print ("\033[0;32m2-back")
+    ml =input ("choose number>>>")
+    if ml =="1" :
+        exit ()
+    if ml =="2" :
+        system ("python Termux_course.py")
+####################################
+if mh =="21" :
+    print ("our team is Consists of two persons")
+    print ("me and the embrator")
+    print ("my number(+201100020487)")
+    print ("ambrator number (+201099621725)")
+    print("thanks for using our tool")
+    print ("\033[0;32m1-exit")
+    print ("\033[0;32m2-back")
+    ml =input ("choose number>>>")
+    if ml =="1" :
+        exit ()
+    if ml =="2" :
+        system ("python Termux_course.py")    
+###################################
+if mh =="22" :
+    exit()
+ 
